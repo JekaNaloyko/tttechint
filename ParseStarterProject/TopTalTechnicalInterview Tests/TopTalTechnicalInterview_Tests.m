2ffdb50abc3bb650f8fa17d3f9ed48e22e354b84
Ievgen Naloiko <ievgen.naloiko@searchconcepts.ch>
2014-05-12 12:57:03 +0300

    Fix.

---
+++ ParseStarterProject/TopTalTechnicalInterview Tests/TopTalTechnicalInterview_Tests.m
@@ -0,0 +1,34 @@
+//
+//  TopTalTechnicalInterview_Tests.m
+//  TopTalTechnicalInterview Tests
+//
+//  Created by Ievgen Naloiko on 5/12/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TopTalTechnicalInterview_Tests : XCTestCase
+
+@end
+
+@implementation TopTalTechnicalInterview_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
